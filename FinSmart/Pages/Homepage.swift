7a04d1541c140a37cdab36eca344882cb1995fc0
Paul Michael <paullmich28@gmail.com>
2024-03-29 18:10:10 +0700

    First Commit

--- FinSmart/Pages/Homepage.swift
+++ FinSmart/Pages/Homepage.swift
@@ -5,4 +5,46 @@
 //  Created by Paulus Michael on 29/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct Homepage: View{
+    var body: some View{
+        VStack(alignment: .leading){
+            HStack{
+                Spacer()
+                Image("NotifIcon")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                Spacer()
+                    .frame(width: 15)
+                Image("SettingIcon")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+            }
+            
+            Spacer()
+                .frame(height: 30)
+            
+            VStack(spacing: 8){
+                Text("Hi,")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .fontWeight(.semibold)
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                
+                Text("Please fill your income and expenses to count your loan")
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            }
+            .padding()
+            .frame(height: 150)
+            .background(.green)
+            .clipShape(RoundedRectangle(cornerRadius: 10.0))
+            
+        }
+        .padding([.horizontal], 30)
+        .frame(minWidth: 0, maxHeight: .infinity, alignment: .topLeading)
+    }
+}
+
+#Preview {
+    Homepage()
+}
