03d9dd019ac2fd80cb673574322c5de644b522d9
Paul Michael <paullmich28@gmail.com>
2024-03-29 17:10:34 +0700

    Initial Commit

---
+++ FinSmart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinSmart
+//
+//  Created by Paulus Michael on 29/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinSmart/FinSmartApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinSmartApp.swift
+//  FinSmart
+//
+//  Created by Paulus Michael on 29/03/24.
+//
+
+import SwiftUI
+
+@main
+struct FinSmartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
