ec2291b5ca6bc1678262d582ac9250a78fae8b4f
Gusti R. Fajar <gustirizkyfajar@outlook.com>
2024-04-02 21:45:24 +0700

    Update finance summary

--- FinSmart/Pages/Add Loan/FinanceSummary.swift
+++ FinSmart/Pages/Add Loan/FinanceSummary.swift
@@ -9,86 +9,131 @@ import SwiftUI
 
 struct FinanceSummary: View {
     var body: some View {
-        NavigationStack{
-            VStack{
+        NavigationStack {
+            VStack {
+				ProgressView(value: 1, total: 6)
+					.padding(.bottom, 16)
                 Text("Finance Summary")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .font(.title)
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                
-                
-                VStack{
-                    VStack{
-                        Text("Your Income:")
-                            .font(.title2)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                        Text("Rp. 5.000.000")
-                            .font(.title3)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                    }
-                    
-                    VStack{
-                        Text("Your Expenses:")
-                            .font(.title2)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                        Text("Rp. 2.000.000")
-                            .font(.title3)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                    }
-                    .padding(.top, 5)
-                }
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 10)
-                    .fill(.green)
-                    .compositingGroup()
-                    .shadow(radius: 3)
-                )
-                
-                VStack{
-                    VStack{
-                        Text("Your Ongoing loans:")
-                            .font(.title2)
-                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-                        
-                        HStack{
-                            Text("Birthday")
-                                .font(.title2)
-                            Text("Rp. 1.000.000")
-                                .font(.title3)
-                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
-                        }
-                        
-                        HStack{
-                            Text("Lunch")
-                                .font(.title2)
-                            Text("Rp. 1.000.000")
-                                .font(.title3)
-                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
-                        }
-                    }
-                }
-                .padding()
-                .background(RoundedRectangle(cornerRadius: 10)
-                    .fill(.orange)
-                    .compositingGroup()
-                    .shadow(radius: 3)
-                )
+				
+				VStack {
+					VStack {
+						Text("Income")
+							.font(.callout)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+						Text("Rp. 5.000.000")
+							.font(.title2)
+							.fontWeight(.semibold)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+					}
+					
+					.padding(.bottom, 5)
+					
+					VStack {
+						Text("Expenses")
+							.font(.callout)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+						Text("Rp. 2.000.000")
+							.font(.title2)
+							.fontWeight(.semibold)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+					}
+				}
+				.padding()
+				.background(RoundedRectangle(cornerRadius: 10)
+					.fill(Color.blue10)
+					.compositingGroup()
+					.shadow(radius: 3)
+				)
+				
+				.padding(.bottom, 12)
+				
+				VStack {
+					VStack {
+						Text("Your Ongoing Installments")
+							.font(.callout)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+						Text("Rp. 1.400.000")
+							.font(.title2)
+							.fontWeight(.semibold)
+							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+					}
+					.padding(.bottom, 16)
+					
+					VStack {
+						HStack {
+							Text("Diamond FreeFire")
+							Text("Rp. 400.000")
+								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+						}
+						
+						Divider()
+						
+						HStack {
+							Text("Diamond Mobile Legend")
+							Text("Rp. 500.000")
+								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+						}
+						
+						Divider()
+						
+						HStack {
+							Text("Earbuds Pro")
+							Text("Rp. 500.000")
+								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+						}
+
+					}
+				}
+				.padding()
+				.background(RoundedRectangle(cornerRadius: 10)
+					.fill(Color.blue10)
+					.compositingGroup()
+					.shadow(radius: 3)
+				)
+				
+				.padding(.bottom, 12)
+				
+				VStack {
+					Text("Net Income")
+						.font(.callout)
+						.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+					Text("Rp. 600.000")
+						.font(.title2)
+						.fontWeight(.semibold)
+						.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+				}
+				.padding()
+				.background(RoundedRectangle(cornerRadius: 10)
+					.fill(Color.yellow50)
+					.compositingGroup()
+					.shadow(radius: 3)
+				)
+				
+				.padding(.bottom, 16)
+				
+				NavigationLink(destination: TitleAmount()) {
+					Text("Start Loan Projection")
+						.font(.headline)
+						.foregroundStyle(Color.white)
+				}
+				.buttonStyle(PlainButtonStyle())
+				.padding()
+				.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+				.background(RoundedRectangle(cornerRadius: 30)
+					.fill(Color.primaryBlue)
+					.shadow(radius: 3)
+				)
             }
             .padding()
-            
-            Spacer()
-            
-            NavigationLink(destination: TitleAmount()){
-                Text("Start Making Loan Projection")
-                    .font(.title3)
-            }
-            .buttonStyle(PlainButtonStyle())
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 10)
-                .fill(.bar)
-                .shadow(radius: 3)
-            )
-            .offset(y: -50.0)
+			.toolbar {
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Image(systemName: "x.circle")
+						.foregroundStyle(Color.primaryBlue)
+				}
+			}
         }
     }
 }
