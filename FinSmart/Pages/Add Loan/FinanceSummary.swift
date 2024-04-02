409512617908e883e2dfe137a022f9236782532a
Paul Michael <paullmich28@gmail.com>
2024-04-02 22:42:39 +0700

    Commit aja

--- FinSmart/Pages/Add Loan/FinanceSummary.swift
+++ FinSmart/Pages/Add Loan/FinanceSummary.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct FinanceSummary: View {
     var body: some View {
+        
         NavigationStack {
             VStack {
 				ProgressView(value: 1, total: 5)
@@ -17,120 +18,126 @@ struct FinanceSummary: View {
                     .font(.title)
                     .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-				
-				VStack {
-					VStack {
-						Text("Income")
-							.font(.callout)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-						Text("Rp. 5.000.000")
-							.font(.title2)
-							.fontWeight(.semibold)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-					}
-					
-					.padding(.bottom, 5)
-					
-					VStack {
-						Text("Expenses")
-							.font(.callout)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-						Text("Rp. 2.000.000")
-							.font(.title2)
-							.fontWeight(.semibold)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-					}
-				}
-				.padding()
-				.background(RoundedRectangle(cornerRadius: 10)
-					.fill(Color.blue10)
-					.compositingGroup()
-					.shadow(radius: 3)
-				)
-				
-				.padding(.bottom, 12)
-				
-				VStack {
-					VStack {
-						Text("Your Ongoing Installments")
-							.font(.callout)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-						Text("Rp. 1.400.000")
-							.font(.title2)
-							.fontWeight(.semibold)
-							.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-					}
-					.padding(.bottom, 16)
-					
-					VStack {
-						HStack {
-							Text("Diamond FreeFire")
-							Text("Rp. 400.000")
-								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
-						}
-						
-						Divider()
-						
-						HStack {
-							Text("Diamond Mobile Legend")
-							Text("Rp. 500.000")
-								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
-						}
-						
-						Divider()
-						
-						HStack {
-							Text("Earbuds Pro")
-							Text("Rp. 500.000")
-								.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
-						}
-
-					}
-				}
-				.padding()
-				.background(RoundedRectangle(cornerRadius: 10)
-					.fill(Color.blue10)
-					.compositingGroup()
-					.shadow(radius: 3)
-				)
-				
-				.padding(.bottom, 12)
-				
-				VStack {
-					Text("Net Income")
-						.font(.callout)
-						.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-					Text("Rp. 600.000")
-						.font(.title2)
-						.fontWeight(.semibold)
-						.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-				}
-				.padding()
-				.background(RoundedRectangle(cornerRadius: 10)
-					.fill(Color.yellow50)
-					.compositingGroup()
-					.shadow(radius: 3)
-				)
-				
-				.padding(.bottom, 16)
-				
-				Spacer()
-				
-				NavigationLink(destination: TitleAmount()) {
-					Text("Start Loan Projection")
-						.font(.headline)
-						.foregroundStyle(Color.white)
-				}
-				.buttonStyle(PlainButtonStyle())
-				.padding()
-				.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-				.background(RoundedRectangle(cornerRadius: 30)
-					.fill(Color.primaryBlue)
-					.shadow(radius: 3)
-				)
+                
+                VStack {
+                    VStack {
+                        Text("Income")
+                            .font(.callout)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                        Text("Rp. 5.000.000")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    }
+                    
+                    .padding(.bottom, 5)
+                    
+                    VStack {
+                        Text("Expenses")
+                            .font(.callout)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                        Text("Rp. 2.000.000")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    }
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.blue10)
+                    .compositingGroup()
+                    .shadow(radius: 3)
+                )
+                
+                .padding(.bottom, 12)
+                
+                VStack {
+                    VStack {
+                        Text("Your Ongoing Installments")
+                            .font(.callout)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                        Text("Rp. 1.400.000")
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    }
+                    .padding(.bottom, 16)
+                    
+                    VStack {
+                        HStack {
+                            Text("Diamond FreeFire")
+                            Text("Rp. 400.000")
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Diamond Mobile Legend")
+                            Text("Rp. 500.000")
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+                        }
+                        
+                        Divider()
+                        
+                        HStack {
+                            Text("Earbuds Pro")
+                            Text("Rp. 500.000")
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .trailing)
+                        }
+                        
+                    }
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.blue10)
+                    .compositingGroup()
+                    .shadow(radius: 3)
+                )
+                
+                .padding(.bottom, 12)
+                
+                VStack {
+                    Text("Net Income")
+                        .font(.callout)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                    Text("Rp. 600.000")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+                }
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.yellow50)
+                    .compositingGroup()
+                    .shadow(radius: 3)
+                )
+                
+                .padding(.bottom, 16)
+                
+                NavigationLink(destination: TitleAmount()) {
+                    Text("Start Loan Projection")
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .padding()
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                .background(RoundedRectangle(cornerRadius: 30)
+                    .fill(Color.primaryBlue)
+                    .shadow(radius: 3)
+                )
             }
             .padding()
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "x.circle")
+                        .foregroundStyle(Color.primaryBlue)
+                }
+                
+            }
         }
+        .offset(y:-50)
     }
 }
 
--- FinSmart/Pages/Add Loan/TitleAmount.swift
+++ FinSmart/Pages/Add Loan/TitleAmount.swift
@@ -8,66 +8,68 @@
 import SwiftUI
 
 struct TitleAmount: View {
-	
-	@State private var loanTitle: String = ""
-	@State private var loanAmount: String = ""
-	
-	var body: some View {
-		NavigationStack {
-			VStack {
-				ProgressView(value: 2, total: 5)
-					.padding(.bottom, 16)
-				Text("Loan Overview")
-					.font(.title)
-					.fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-					.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
-				
-					.padding(.bottom, 18)
-				
-				VStack (alignment: .leading) {
-					Text("What do you want to use the loan for?")
-						.font(.callout)
-					TextField("Loan title", text: $loanTitle)
-						.disableAutocorrection(true)
-						.padding()
-						.overlay(
-							RoundedRectangle(cornerRadius: 10)
-								.stroke(Color.primaryBlue)
-					)
-				}
-				.padding(.bottom, 16)
-				
-				VStack (alignment: .leading) {
-					Text("How much do you want to loan?")
-						.font(.callout)
-					TextField("Loan amount", text: $loanTitle)
-						.disableAutocorrection(true)
-						.padding()
-						.overlay(
-							RoundedRectangle(cornerRadius: 10)
-								.stroke(Color.primaryBlue)
-					)
-				}
-				.padding(.bottom, 32)
-				
-				Spacer()
-				
-				NavigationLink(destination: PlatformListProjection()) {
-					Text("Continue")
-						.font(.headline)
-						.foregroundStyle(Color.white)
-				}
-				.buttonStyle(PlainButtonStyle())
-				.padding()
-				.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-				.background(RoundedRectangle(cornerRadius: 30)
-					.fill(Color.primaryBlue)
-					.shadow(radius: 3)
-				)
-			}
-			.padding()
-		}
-	}
+    @State private var loanTitle: String = ""
+    @State private var loanAmount: String = ""
+    
+    var body: some View {
+        VStack{
+            ProgressView(value: 2, total: 5)
+            
+            Text("Loan Overview")
+                .padding([.vertical])
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                .fontWeight(.bold)
+                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .leading)
+            
+            VStack {
+                VStack(alignment: .leading){
+                    Text("Loan Title")
+                    TextField(
+                        "Loan Title",
+                        text: $loanTitle
+                    )
+                    .disableAutocorrection(true)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.blue)
+                    )
+                
+                }.padding([.bottom])
+                
+                VStack(alignment: .leading){
+                    Text("Loan Amount")
+                    TextField(
+                        "Loan Amount",
+                        text: $loanAmount
+                    )
+                    .disableAutocorrection(true)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.blue)
+                    )
+                }
+                
+            }
+            .textFieldStyle(.roundedBorder)
+            
+            Spacer()
+            
+            NavigationLink(destination: PlatformListProjection()){
+                Text("Continue")
+                    .font(.title3)
+                    .foregroundColor(.white)
+                    .fontWeight(.semibold)
+            }
+            .buttonStyle(PlainButtonStyle())
+            .padding()
+            .background(RoundedRectangle(cornerRadius: 10)
+                .fill(.primaryBlue)
+                .shadow(radius: 3)
+            )
+            .offset(y: -50.0)
+        }
+        .padding()
+    }
 }
 
 #Preview {
