e3ce6cea87a2da7d721abbb7f805236ef58d9028
Filbert C <filbertc2@gmail.com>
2024-04-02 10:18:10 +0700

    notification page

--- FinSmart/Pages/NotificationList.swift
+++ FinSmart/Pages/NotificationList.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct NotificationList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack (alignment: .leading, spacing: 5) {
+                
+            }
+            .background(.white)
+            .scrollContentBackground(.hidden)
+            .navigationTitle("Notification")
+        }
     }
 }
 
