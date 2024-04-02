b0c4b8c827e8f3b002dcfce185c2863fea812eca
Paul Michael <paullmich28@gmail.com>
2024-04-02 23:55:30 +0700

    Gatau males pengen beli truk

---
+++ FinSmart/Pages/OnboardingPage.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingPage.swift
+//  FinSmart
+//
+//  Created by Paulus Michael on 02/04/24.
+//
+
+import SwiftUI
+
+struct OnboardingPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingPage()
+}
---
+++ FinSmart/Pages/SplashScreen.swift
@@ -0,0 +1,18 @@
+//
+//  SplashScreen.swift
+//  FinSmart
+//
+//  Created by Paulus Michael on 02/04/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
